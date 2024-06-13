829faca0ab3d267ea9cbb232e1edb3291b5d8fe0
Djo65 <joriscamus@hotmail.fr>
2024-06-13 16:06:31 +0200

    structure page 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <img
+          className="photoProfile"
+          src="https://vignette.wikia.nocookie.net/fr.starwars/images/3/32/Dark_Vador.jpg"
+          alt="#"
+        />
+        <div className="detailsProfils">
+          <div className="detailProfil">
+            <ul>
+              <li>nom</li>
+              <li>taille</li>
+              <li>age</li>
+            </ul>
+          </div>
+          <div className="detailProfil">
+            <ul>
+              <li>genre</li>
+              <li>planete</li>
+              <li>espece</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="likes">
+          <div className="like">
+            <img
+              id="imgLike"
+              src="../src/assets/ewok_icon-icons.com_76943.svg"
+              alt=""
+            />
+            <p>Like</p>
+          </div>
+          <div className="like">
+            <img
+              id="imgDislike"
+              src="../src/assets/jabba-the-hutt_icon-icons.com_76950.svg"
+              alt=""
+            />
+            <p>dislike</p>
+          </div>
+        </div>
+        <button>Envoyer message</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
