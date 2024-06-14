59f08e825b7ac2930b7f9d7f7c2ca991849199cd
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-06-14 14:23:47 +0200

    CEST ENFIN FINI

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,30 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import ProfileNav from './components/ProfileNav.jsx';
 import Radar from "./components/Radar";
-import { useEffect ,useState  } from "react";
+import { useEffect, useState } from "react";
 import Pub from './components/Pub.jsx';
 
 import './App.css';
 
 const App = () => {
-  const [starWarsCharacters, setStarWarsCharacters] = useState([]);
-  useEffect(() => {
-    fetch("https://miadil.github.io/starwars-api/api/all.json")
-      .then((result) => result.json())
-      .then((result) => setStarWarsCharacters(result));
-  }, []);
+    const [starWarsCharacters, setStarWarsCharacters] = useState([]);
+    useEffect(() => {
+        fetch("https://miadil.github.io/starwars-api/api/all.json")
+            .then((result) => result.json())
+            .then((result) => setStarWarsCharacters(result));
+    }, []);
 
-  const getRandomElements = (arr, num) => {
-    const shuffled = [...arr].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-  };
+    const getRandomElements = (arr, num) => {
+        const shuffled = [...arr].sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, num);
+    };
 
-  const filteredCharacters = getRandomElements(starWarsCharacters, 3);
+    const filteredCharacters = getRandomElements(starWarsCharacters, 3);
 
     return (
-        <div className="app">       
-                <Header />
-                <div className='profiles-images'>               
+        <div className="app">
+            <Header />
+            <div className='profiles-images'>
                 {filteredCharacters.map((character) => {
                     return (
                         <ProfileNav
@@ -34,15 +34,15 @@ const App = () => {
                             id={character.id}
                             name={character.name} />
                     );
-                })}   
-                </div>
-                <Pub />
-                
+                })}
+            </div>
+            <Pub />
+
             <div className="app-body">
                 <Radar />
                 <Footer />
             </div>
-           
+
         </div>
     );
 };
