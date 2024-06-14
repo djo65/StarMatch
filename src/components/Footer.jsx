905074ffb3d365d0a3c764ddb64a89f5e67f7df5
ALIX CHAMPAGNOL <alix.champagnol@gmail.com>
2024-06-14 14:08:07 +0200

    final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import './Footer.css';
+import chewieSound from '../assets/chewie.mp3';
+import r2d2Sound from '../assets/r2d2.mp3';
+import saberSound from '../assets/saber.mp3';
+import c3poSound from '../assets/c3po.mp3';
 
 const Footer = () => {
+
+  const playSound = (sound) => {
+    new Audio(sound).play();
+  };
+
   return (
     <div className="footer">
-      <div className="footer-item">
+      <div className="footer-item" onClick={() => playSound(chewieSound)}>
         <h3>Profil</h3>
-        <img src="src\assets\chewbacca.svg" className="bottom-left-logo" alt="Logo" />
+        <img src="src/assets/chewbacca.svg" className="bottom-left-logo" alt="Logo" />
       </div>
-      <div className="footer-item">
+      <div className="footer-item" onClick={() => playSound(r2d2Sound)}>
         <h3>Message</h3>
-        <img src="src\assets\r2d2.svg" className="bottom-secund-logo" alt="Logo" />
+        <img src="src/assets/r2d2.svg" className="bottom-secund-logo" alt="Logo" />
       </div>
-      <div className="footer-item">
+      <div className="footer-item" onClick={() => playSound(saberSound)}>
         <h3>Match</h3>
-        <img src="src\assets\ligthsaber.svg" className="bottom-third-logo" alt="Logo" />
+        <img src="src/assets/ligthsaber.svg" className="bottom-third-logo" alt="Logo" />
       </div>
-      <div className="footer-item">
+      <div className="footer-item" onClick={() => playSound(c3poSound)}>
         <h3>Parametre</h3>
-        <img src="src\assets\c3p0.svg" className="bottom-fourth-logo" alt="Logo" /> 
-            
-            
-            
-        
+        <img src="src/assets/c3p0.svg" className="bottom-fourth-logo" alt="Logo" />
       </div>
     </div>
   );
